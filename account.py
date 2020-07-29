e598b0d677c651231764834060dc08c1c6820882
njambibetty <njambiwan@gmail.com>
2020-07-30 02:35:19 +0300

    Added datetime,exception and error handling

--- account.py
+++ account.py
@@ -1,29 +1,86 @@
 class BankAccount:
-  bank = "Barclays"
+  #bank = "Barclays"
 
 
-  def __init__(self, first_name, last_name,phone_number):
+  def __init__(self, first_name, last_name, phone_number,bank):
     self.first_name = first_name
     self.last_name = last_name
     self.balance = 0
     self.phone_number = phone_number
+    self.bank= bamk
+    self.deposits = []
+    self.withdrawals= []
     
+    
+
   def account_name(self):
     name = "{} account for {} {}".format(
       self.bank, self.first_name, self.last_name)
     return name
+
   def deposit(self,amount):
-    if amount > 0:
-     self.balance += amount
-     print("You have deposited {} to your account".format(amount))
+    try:
+      amount + 1
+    except TypeError:
+      print("You must enter the amounnt in figures")
+      return
     else:
-      print("Kindly,you cannot deposit {} to your account".format(amount))
+      self.balance += amount
+      self.deposits.append(amount)
+      print("You have deposited {} to {}".format(amount, self.account_name))
+
   def withdraw(self,amount):
-    if amount > 0:
-     self.balance -= amount
-     print("You have withdrawn {} from your account".format(amount))
+    try:
+      amount + 1
+    except TypeError:
+      print("You must enter the amount in figures")
+    if amount <= 0:
+      print("You cannot withdraw zero or negative")
+    elif amount > self.balance:
+       print("You don't have enough balance")
     else:
-      print("Kindly,you cannot withdraw {} from your account".format(amount))
+      self.balance -= amount
+      self.withdrawals.append(amount)
+      print("You have withdrawn {} from {}".format(amount, self.account_names))
+
   def get_balance(self):
     return "The balance for {} is {}".format(
       self.account_name(), self.balance)
+  
+  def show_deposit_statements(self,amount):
+     for deposit in self.deposits:
+       print(deposit)
+
+  def show_withdrawals_statement(self):
+    for withdrawal in self.withdrawals:
+      print(withdrawal)
+
+  def request_loan(self,amount):
+    try:
+      amount + 1
+    except TypeError:
+      print("You must enter the amount in figures")
+    if amount <= 0:
+      try:
+        amount + 1
+      except TypeError:
+        print("The amount should be in figures")
+        return
+      print("You cannot request a loan of that amount")
+    else:
+        self.loan = amount
+        print("You have be a given a loan of {}".format(amount))
+
+  def repay_loan(self,amount):
+    try:
+      amount + 1
+    except TypeError:
+      print("The amount should be in figures")
+      return
+    if amount <= 0:
+      print("You cannot repay with that amount")
+    elif amount < self.loan:
+        print("Your loan is {}, please enter an amount less or equal".format(self.loan))
+    else:
+        self.loan -= amount
+        print("You have repaid your loan with {}.Your loan is {}".format(amount,self.loan))
